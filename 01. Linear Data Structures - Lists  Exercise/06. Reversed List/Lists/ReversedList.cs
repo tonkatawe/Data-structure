6a9f0e7e3a61d44db49d70c92e1d34d6623a9aaf
tonkatawe <vlahov.anton@gmail.com>
2020-06-02 17:33:36 +0300

    Data Structure

---
+++ 01. Linear Data Structures - Lists  Exercise/06. Reversed List/Lists/ReversedList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class ReversedList<T> : IEnumerable<T>
+{
+    public int Count { get; private set; }
+    private T[] elements;
+
+    public ReversedList()
+    {
+        this.Capacity = 2;
+        this.elements = new T[Capacity];
+    }
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return this.elements[this.Count - index - 1];
+        }
+
+        set
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            this.elements[this.Count - 1 - index] = value;
+        }
+    }
+    public int Capacity { get; set; }
+
+    public void Add(T item)
+    {
+        if (elements.Length == this.Count)
+        {
+            this.Resize();
+        }
+
+        this.elements[this.Count++] = item;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        T element = this[index];
+
+        for (int i = this.Count - index - 1; i < this.Count - 1; i++)
+        {
+            this.elements[i] = this.elements[i + 1];
+        }
+
+        this.Count--;
+
+        return element;
+    }
+    public void Resize()
+    {
+        var newElements = new T[this.Capacity * 2];
+        Array.Copy(this.elements, newElements, this.Count);
+        this.Capacity *= 2;
+        this.elements = newElements;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = this.Count - 1; i >= 0; i--)
+        {
+            yield return this.elements[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- 01. Linear Data Structures - Lists  Exercise/07. Distance in Labyrinth/Program.cs
+++ 01. Linear Data Structures - Lists  Exercise/07. Distance in Labyrinth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07._Distance_in_Labyrinth
 {
@@ -6,7 +7,111 @@ namespace _07._Distance_in_Labyrinth
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size = int.Parse(Console.ReadLine());
+
+            var matrix = new string[size, size];
+            //make the same size bool matrix
+            var visited = new bool[size, size];
+
+            var row = 0;
+            var col = 0;
+            //Read the matrix
+            for (var matrixRow = 0; matrixRow < matrix.GetLength(0); matrixRow++)
+            {
+                var currentRowInMatrix = Console.ReadLine()
+                    .ToCharArray();
+
+                for (var matrixCol = 0; matrixCol < matrix.GetLength(1); matrixCol++)
+                {
+                    matrix[matrixRow, matrixCol] = currentRowInMatrix[matrixCol].ToString();
+                    if (currentRowInMatrix[matrixCol] == '*')
+                    {
+                        row = matrixRow;
+                        col = matrixCol;
+                    }
+                }
+            }
+
+
+            var checker = new Queue<Cell>();
+            checker.Enqueue(new Cell(row, col, false, 0));
+
+            while (checker.Count != 0)
+            {
+                //take currentCell
+                var currentCell = checker.Dequeue();
+                row = currentCell.Row;
+                col = currentCell.Col;
+
+                currentCell.Visited = true;
+                visited[row, col] = true;
+                if (matrix[row, col] != "*")
+                {
+
+                    matrix[row, col] = currentCell.Moves.ToString();
+                }
+
+                //move up
+                if (row - 1 >= 0 && matrix[row - 1, col] != "x" && !visited[row - 1, col])
+                {
+                    checker.Enqueue(new Cell(row - 1, col, false, currentCell.Moves + 1));
+                }
+                //move down
+                if (row + 1 < matrix.GetLength(0) && matrix[row + 1, col] != "x" && !visited[row + 1, col])
+                {
+                    checker.Enqueue(new Cell(row + 1, col, false, currentCell.Moves + 1));
+                }
+                //move right
+                if (col + 1 < matrix.GetLength(1) && matrix[row, col + 1] != "x" && !visited[row, col + 1])
+                {
+                    checker.Enqueue(new Cell(row, col + 1, false, currentCell.Moves + 1));
+                }
+                //move left
+                if (col - 1 >= 0 && matrix[row, col - 1] != "x" && !visited[row, col - 1])
+                {
+                    checker.Enqueue(new Cell(row, col - 1, false, currentCell.Moves + 1));
+                }
+            }
+
+            PrintMatrix(matrix);
+
+
+        }
+
+        private static void PrintMatrix(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == "0")
+                    {
+                        Console.Write("u");
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        //Use this class for check cells in matrix
+        private class Cell
+        {
+            public Cell(int row, int col, bool visited, int moves)
+            {
+                this.Row = row;
+                this.Col = col;
+                this.Visited = visited;
+                this.Moves = moves;
+            }
+            public int Row { get; set; }
+            public int Col { get; set; }
+            public bool Visited { get; set; }
+            public int Moves { get; set; }
         }
     }
 }
