41543ea924dbaba38037c109454db85fa7b39c24
tonkatawe <vlahov.anton@gmail.com>
2020-06-04 16:50:04 +0300

    Data Structure

---
+++ 03.Basic Tree Data Structures -  Exercise/Trees/PlayWithTrees.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+
+
+class Program
+{
+    static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
+    static void Main(string[] args)
+    {
+        ReadTree();
+        Console.WriteLine($"Root node: {GetRootNode().Value}");
+    }
+
+    static void AddEdge(int parent, int child)
+    {
+        Tree<int> parentNode = GetTreeNodeByValue(parent);
+        Tree<int> childNode = GetTreeNodeByValue(child);
+        parentNode.Children.Add(childNode);
+        childNode.Parent = parentNode;
+
+    }
+
+    static void ReadTree()
+    {
+        int nodeCount = int.Parse(Console.ReadLine());
+        for (int i = 1; i < nodeCount; i++)
+        {
+            string[] edge = Console.ReadLine().Split(' ');
+            AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
+        }
+    }
+
+    static Tree<int> GetRootNode()
+    {
+        return nodeByValue.Values
+            .FirstOrDefault(x => x.Parent == null);
+    }
+    static Tree<int> GetTreeNodeByValue(int value)
+    {
+        if (!nodeByValue.ContainsKey(value))
+        {
+            nodeByValue[value] = new Tree<int>(value);
+        }
+
+        return nodeByValue[value];
+    }
+}
---
+++ 03.Basic Tree Data Structures -  Exercise/Trees/Tree.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>(children);
+    }
+    public T Value { get; set; }
+    public Tree<T> Parent { get; set; }
+    public List<Tree<T>> Children { get; set; }
+}
+
