8ee9623c8a373f3df0d295b8d23333f7b35fab33
tonkatawe <vlahov.anton@gmail.com>
2020-07-17 11:40:20 +0300

    Data structure

---
+++ 08.Ropes and Tries - Exercise/Trie/StringEditor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+
+    public class StringEditor : ITextEditor
+    {
+        private Trie<BigList<char>> usersStrings;
+        private Trie<Stack<string>> usersStack;
+
+        public StringEditor()
+        {
+            this.usersStrings = new Trie<BigList<char>>();
+            this.usersStack = new Trie<Stack<string>>();
+
+        }
+        public void Login(string username)
+        {
+            this.usersStack.Insert(username, new Stack<string>());
+            this.usersStrings.Insert(username, new BigList<char>());
+        }
+
+        public void Logout(string username)
+        {
+            if (this.usersStrings.Contains(username))
+            {
+                this.usersStrings.Delete(username);
+                this.usersStack.Delete(username);
+
+            }
+        }
+
+        public void Prepend(string username, string str)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(string.Join("", this.usersStrings.GetValue(username)));
+            this.usersStrings.GetValue(username).AddRangeToFront(str);
+        }
+
+        public void Insert(string username, int index, string str)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(string.Join("", this.usersStrings.GetValue(username)));
+
+            this.usersStrings.GetValue(username).InsertRange(index, str);
+        }
+
+        public void Substring(string username, int startIndex, int length)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+            this.usersStack.GetValue(username).Push(string.Join("", this.usersStrings.GetValue(username)));
+
+            var userString = this.usersStrings.GetValue(username);
+            var userHistory = this.usersStack.GetValue(username);
+            userHistory.Push(string.Join("", userString));
+
+            this.usersStrings.GetValue(username).GetRange(startIndex, length);
+        }
+
+        public void Delete(string username, int startIndex, int length)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(string.Join("", this.usersStrings.GetValue(username)));
+
+            this.usersStrings.GetValue(username).RemoveRange(startIndex, length);
+        }
+
+        public void Clear(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+            this.usersStack.GetValue(username).Push(string.Join("", this.usersStrings.GetValue(username)));
+
+            this.usersStrings.Insert(username, new BigList<char>());
+        }
+
+        public int Length(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Print(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return " ";
+            }
+
+            return string.Join(" ", this.usersStrings.GetValue(username));
+        }
+
+        public void Undo(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            var userString = this.usersStrings.GetValue(username);
+            var userHistory = this.usersStack.GetValue(username);
+
+            if (userHistory.Count == 0)
+            {
+                return;
+            }
+            var lastUserString = userHistory.Pop();
+            userHistory.Push(string.Join("", userString));
+
+            this.usersStrings.Insert(username, new BigList<char>(lastUserString));
+        }
+
+        public IEnumerable<string> Users(string prefix = "")
+        {
+            throw new NotImplementedException();
+        }
+    }
+
---
+++ 08.Ropes and Tries - Exercise/Trie/TriePerformanceTester.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+
+public class TriePerformanceTester
+{
+    private const string VocabularyPath = "vocabulary.txt";
+
+    public static void Main()
+    {
+     var stringEditor = new StringEditor();
+     stringEditor.Login("pesho");
+     stringEditor.Prepend("pesho", "stringexample");
+     stringEditor.Delete("pesho",3,6);
+     stringEditor.Insert("pesho", 3, "asdassdaasd");
+     Console.WriteLine(stringEditor.Print("pesho"));
+    }
+
+    private static IEnumerable<string> GetAllMatches(char[] chars, int length)
+    {
+        int[] indexes = new int[length];
+        char[] current = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            current[i] = chars[0];
+        }
+
+        do
+        {
+            yield return new string(current);
+        }
+        while (Increment(indexes, current, chars));
+    }
+
+    private static bool Increment(int[] indexes, char[] current, char[] chars)
+    {
+        int position = indexes.Length - 1;
+
+        while (position >= 0)
+        {
+            indexes[position]++;
+            if (indexes[position] < chars.Length)
+            {
+                current[position] = chars[indexes[position]];
+                return true;
+            }
+
+            indexes[position] = 0;
+            current[position] = chars[0];
+            position--;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Returns distinct set of words. <remarks>This method returns 58110 English words.</remarks>
+    /// </summary>
+    /// <returns>Distinct set of words.</returns>
+    private static IEnumerable<string> LoadWords(string fileName)
+    {
+        string path = Path.Combine(@"..\..\", fileName);
+        return File.ReadAllLines(path);
+    }
+}
